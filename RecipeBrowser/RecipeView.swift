044dcea58ddf827c3b5501ca8e956dd072e68cd5
aabagdi <70984836+aabagdi@users.noreply.github.com>
2023-04-25 23:39:06 -0700

    Added clickable links to source recipes

--- RecipeBrowser/RecipeView.swift
+++ RecipeBrowser/RecipeView.swift
@@ -14,45 +14,50 @@ struct RecipeView: View {
     let mealID : String
     
     var body: some View {
+        let imageURL =  "\((recipeSteps["strSource"] ?? "unknown")!)"
+        
         Text((recipeSteps["strMeal"] ?? "loading" )!)
             .font(.headline)
-        List {
-            Section {
-                VStack {
-                    HStack {
-                        Spacer()
-                        AsyncImage(url: URL(string: (recipeSteps["strMealThumb"] ?? "https://user-images.githubusercontent.com/42506001/221354918-01bf0e89-48be-4df7-85bb-cdf5d0136f2a.png" )!)!, scale: 50.0) { image in image.resizable() } placeholder: { Color.gray } .frame(width: 300, height: 300) .clipShape(RoundedRectangle(cornerRadius: 10))
-                        Spacer()
+        GeometryReader { g in
+            List {
+                Section {
+                    VStack {
+                        HStack {
+                            Spacer()
+                            AsyncImage(url: URL(string: (recipeSteps["strMealThumb"] ?? "https://user-images.githubusercontent.com/42506001/221354918-01bf0e89-48be-4df7-85bb-cdf5d0136f2a.png" )!)!, scale: 50.0) { image in image.resizable() } placeholder: { Color.gray } .frame(width: g.size.width * 0.7633587786, height: g.size.width * 0.7633587786) .clipShape(RoundedRectangle(cornerRadius: 10))
+                            Spacer()
+                        }
+                        Text("A delicious \((recipeSteps["strArea"] ?? "loading")!) \((recipeSteps["strCategory"] ?? "loading")!.lowercased())!")
+                            .font(.caption)
+                        Link("Click here for recipe video!", destination: URL(string: (recipeSteps["strYoutube"] ?? "loading")!)!)
                     }
-                    Text("A delicious \((recipeSteps["strArea"] ?? "loading")!) \((recipeSteps["strCategory"] ?? "loading")!.lowercased())!")
+                }
+                
+                Section(header: Text("You will need:")) {
+                    ForEach(Array(ingredients.keys.enumerated()), id: \.element) { _, ingredient in
+                        if let amount = ingredients[ingredient] {
+                            Text("**\(ingredient)**: \(amount)")
+                        } else {
+                            Text("no description")
+                        }
+                    }
+                }
+                .headerProminence(.increased)
+                
+                Section {
+                    Text((recipeSteps["strInstructions"] ?? "loading")!)
+                } header: {
+                    Text("Recipe:")
+                        .headerProminence(.increased)
+                } footer: {
+                    Text("Recipe courtesy of: \(try! AttributedString(markdown: imageURL))")
                         .font(.caption)
-                    Link("Click here for recipe video!", destination: URL(string: (recipeSteps["strYoutube"] ?? "loading")!)!)
                 }
             }
-            
-            Section(header: Text("You will need:")) {
-                ForEach(Array(ingredients.keys.enumerated()), id: \.element) { _, ingredient in
-                    if let amount = ingredients[ingredient] {
-                        Text("**\(ingredient)**: \(amount)")
-                    } else {
-                        Text("no description")
-                    }
-                }
-            }
-            .headerProminence(.increased)
-            
-            Section(header: Text("Recipe:")) {
-                Text((recipeSteps["strInstructions"] ?? "loading")!)
-            }
-            .headerProminence(.increased)
-            
-            Section("Recipe courtesy of \((recipeSteps["strSource"] ?? "unknown")!)") {
+            .task {
+                await loadRecipe()
             }
         }
-        .task {
-            await loadRecipe()
-        }
-        
     }
     
     func loadRecipe() async {
